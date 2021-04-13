1e8be38170c761cf678745b8551931931c120890
VildanovMM <maratvildan153@gmail.com>
2021-04-13 13:10:03 +0300

    доработан тест

--- src/test/java/tddkata/CalcTest.java
+++ src/test/java/tddkata/CalcTest.java
@@ -7,16 +7,42 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class CalcTest {
 
     @Test
-    public void test() {
+    public void testEmpty() {
         Calc calc = new Calc();
 
         int res = calc.sum("");
         assertEquals(0, res);
+    }
 
-        res = calc.sum("1");
+    @Test
+    public void testOne() {
+        Calc calc = new Calc();
+
+        int res = calc.sum("1");
         assertEquals(1, res);
+    }
 
-        res = calc.sum("1,2");
-        assertEquals(3, res);
+    @Test
+    public void testTwoElements() {
+        Calc calc = new Calc();
+
+        int res = calc.sum("1,2");
+        assertEquals(0, res);
+    }
+
+    @Test
+    public void testIncorrectNums1() {
+        Calc calc = new Calc();
+
+        int res = calc.sum("1,a");
+        assertEquals(-1, res);
+    }
+
+    @Test
+    public void testIncorrectNums2() {
+        Calc calc = new Calc();
+
+        int res = calc.sum("abc,2");
+        assertEquals(-1, res);
     }
 }
