c2a11cfb66efd773ad0f47a26ea5a600b0f3e677
VildanovMM <maratvildan153@gmail.com>
2021-04-13 13:31:36 +0300

    добавлен код

--- src/main/java/tddkata/Calc.java
+++ src/main/java/tddkata/Calc.java
@@ -1,7 +1,26 @@
 package tddkata;
 
+import org.apache.commons.lang3.StringUtils;
+
 public class Calc {
+    private static final Integer INCORRECT = -1;
+
     public int sum(String expression) {
-        throw new UnsupportedOperationException();
+        if (expression == null) {
+            return INCORRECT;
+        }
+        if (StringUtils.isEmpty(expression)) {
+            return 0;
+        }
+        String regex = "\\d{1,}";
+        if (expression.matches(regex)) {
+            return Integer.valueOf(expression);
+        }
+        regex = "\\d{1,},\\d{1,}";
+        if (expression.matches(regex)) {
+            String[] split = StringUtils.split(expression, ",");
+            return Integer.valueOf(split[0]) + Integer.valueOf(split[1]);
+        }
+        return INCORRECT;
     }
 }
--- src/test/java/tddkata/CalcTest.java
+++ src/test/java/tddkata/CalcTest.java
@@ -5,68 +5,68 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CalcTest {
+    Calc calc = new Calc();
+
+    @Test
+    public void testNull() {
+        int expected = -1;
+        int res = calc.sum(null);
+        assertEquals(expected, res);
+    }
 
     @Test
     public void testEmpty() {
-        Calc calc = new Calc();
-
+        int expected = 0;
         int res = calc.sum("");
-        assertEquals(0, res);
+        assertEquals(expected, res);
     }
 
     @Test
     public void testOne() {
-        Calc calc = new Calc();
-
+        int expected = 1;
         int res = calc.sum("1");
-        assertEquals(1, res);
+        assertEquals(expected, res);
     }
 
     @Test
     public void testTwoElements() {
-        Calc calc = new Calc();
-
+        int expected = 3;
         int res = calc.sum("1,2");
-        assertEquals(3, res);
+        assertEquals(expected, res);
     }
 
     @Test
     public void testIncorrectNums1() {
-        Calc calc = new Calc();
-
+        int expected = -1;
         int res = calc.sum("1,a");
-        assertEquals(-1, res);
+        assertEquals(expected, res);
     }
 
     @Test
     public void testIncorrectNums2() {
-        Calc calc = new Calc();
-
+        int expected = -1;
         int res = calc.sum("abc,2");
-        assertEquals(-1, res);
+        assertEquals(expected, res);
     }
 
     @Test
     public void testIncorrectDelimiter1() {
-        Calc calc = new Calc();
-
+        int expected = -1;
         int res = calc.sum("1;2");
-        assertEquals(-1, res);
+        assertEquals(expected, res);
     }
 
     @Test
     public void testIncorrectDelimiter2() {
-        Calc calc = new Calc();
-
+        int expected = -1;
         int res = calc.sum("1:2");
-        assertEquals(-1, res);
+        assertEquals(expected, res);
     }
 
     @Test
     public void testIncorrectDelimiterAndNum() {
-        Calc calc = new Calc();
-
+        int expected = -1;
         int res = calc.sum("1:abc");
-        assertEquals(-1, res);
+        assertEquals(expected, res);
     }
 }
